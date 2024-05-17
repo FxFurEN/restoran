b10506179c3abc3378b214c0cfd0b1b64a0041dd
fxfuren <nikitostimoshenko@gmail.com>
2024-05-17 16:56:12 +0300

    fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,18 @@
+import { LikeOutlined } from "@ant-design/icons";
+
 type CardProps = {
-  image: string;
   title: string;
   span: string;
   description: string;
 };
 
-const Card = ({ image, title, span, description }: CardProps) => {
+const Card = ({title, span, description }: CardProps) => {
   return (
     <div className="bg-white shadow-lg rounded-lg overflow-hidden">
       <div className="p-6">
-        <img src={image} alt={title} className="h-48 w-full object-cover" />
+        <div className="flex items-center justify-center"> {/* Добавляем классы flex и items-center */}
+            <LikeOutlined className="text-orange-500 text-3xl" />
+        </div>
         <h2 className="text-xl font-semibold mt-4">{title} <span className="text-orange-500">{span}</span></h2>
         <p className="text-gray-600 mt-2">{description}</p>
       </div>
