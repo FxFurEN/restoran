658c1012b2723796a179d735da29a93332c70b8b
fxfuren <nikitostimoshenko@gmail.com>
2024-05-17 17:20:06 +0300

    Carousel add

--- src/components/Dishes.tsx
+++ src/components/Dishes.tsx
@@ -1,23 +1,29 @@
+import { Carousel } from 'antd';
+
+const contentStyle: React.CSSProperties = {
+  margin: '0 auto',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  textAlign: 'center',
+};
+
 const Dishes = () => {
+  const dishes = ['pizza.jpg', 'pizza.jpg', 'pizza.jpg', 'pizza.jpg'];
+
+  const carouselItems = dishes.map((dish, index) => (
+    <div key={index}>
+      <img src={dish} alt={`Dish ${index}`} style={contentStyle} />
+    </div>
+  ));
   return (
     <section className="py-16">
       <div className="container mx-auto px-6">
         <h2 className="text-4xl font-bold text-center">Наше <span className="text-orange-500">Меню</span></h2>
-        <div className="mt-8 flex justify-center space-x-8">
-          <div className="w-1/3">
-            <img src="pizza.jpg" alt="Pizza" className="w-full h-auto object-cover" />
-          </div>
-          <div className="flex flex-wrap w-2/3">
-            <div className="w-1/3 p-2">
-              <img src="../../public/burger.jpg" alt="Burger" className="w-full h-auto object-cover" />
-            </div>
-            <div className="w-1/3 p-2">
-              <img src="../../public/burger.jpg" alt="Burger" className="w-full h-auto object-cover" />
-            </div>
-            <div className="w-1/3 p-2">
-              <img src="../../public/burger.jpg" alt="Burger" className="w-full h-auto object-cover" />
-            </div>
-          </div>
+        <div className="mt-8">
+          <Carousel slidesToShow={3} dots={false} autoplay autoplaySpeed={2000}>
+            {carouselItems}
+          </Carousel>
         </div>
       </div>
     </section>
