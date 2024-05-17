8b169525b2ea4c4250f19e10d326e5e4d46e0eab
fxfuren <nikitostimoshenko@gmail.com>
2024-05-17 20:09:56 +0300

    design modal

--- src/components/ReservationModal.tsx
+++ src/components/ReservationModal.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
-import { Modal } from "antd";
+import { Modal, Input, Button, Empty } from "antd";
 
 const ReservationModal = ({ visible, onCancel }) => {
   const [confirmLoading, setConfirmLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState(""); 
+  const [email, setEmail] = useState(""); 
 
   const handleOk = () => {
+    // Здесь можно добавить логику для обработки бронирования
     setConfirmLoading(true);
     setTimeout(() => {
       setConfirmLoading(false);
@@ -23,8 +27,32 @@ const ReservationModal = ({ visible, onCancel }) => {
       onOk={handleOk}
       confirmLoading={confirmLoading}
       onCancel={handleCancel}
+      footer={[
+        <Button key="back" onClick={handleOk} loading={confirmLoading} className="bg-orange-500 text-black border-0">
+          Забронировать
+        </Button>
+      ]}
     >
-      <p>Введите свои данные для бронирования:</p>
+      <div>
+        <h2 className="text-xl font-bold">Оставьте свои контакты для того, чтобы мы могли свами связаться:</h2>
+        <Input
+          placeholder="Ваше ФИО"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          style={{ marginBottom: "10px" }}
+        />
+        <Input
+          placeholder="Номер телефона"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          style={{ marginBottom: "10px" }}
+        />
+        <Input
+          placeholder="Почта"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
     </Modal>
   );
 };
